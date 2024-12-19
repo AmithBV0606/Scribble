f5487370375f4b96658ad58329515d960d0afe72
AmithBV0606 <amithrao0606@gmail.com>
2024-12-19 13:39:27 +0530

    Optimized Image component.

--- client/src/components/FeaturedPost.jsx
+++ client/src/components/FeaturedPost.jsx
@@ -8,13 +8,17 @@ const FeaturedPost = () => {
       {/* First */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* Image */}
-        <Image src="./featured1.jpeg" className="rounded-3xl object-cover" />
+        <Image
+          src="./featured1.jpeg"
+          className="rounded-3xl object-cover"
+          w={"767"}
+        />
 
         {/* Details */}
         <div className="flex items-center gap-4">
           <h1 className="font-semibold lg:text-lg">01.</h1>
 
-          <Link className="text-gray-200">Web Design</Link>
+          <Link className="text-gray-300">Web Design</Link>
 
           <span className="text-gray-300">2 days ago</span>
         </div>
@@ -32,17 +36,20 @@ const FeaturedPost = () => {
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* Second */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className={"rounded-3xl object-cover w-1/3 aspect-video"}
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured2.jpeg"
+              className={"rounded-3xl object-cover w-full h-full"}
+              w="256"
+            />
+          </div>
 
           {/* Details and Title */}
           <div className="w-2/3 my-auto">
             {/* Details */}
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold lg:text-lg">02.</h1>
-              <Link className="text-gray-200">Development</Link>
+              <Link className="text-gray-300">Development</Link>
               <span className="text-gray-300 text-sm">3 days ago</span>
             </div>
 
@@ -58,17 +65,20 @@ const FeaturedPost = () => {
 
         {/* Third */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured3.jpeg"
-            className={"rounded-3xl object-cover w-1/3 aspect-video"}
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured3.jpeg"
+              className={"rounded-3xl object-cover w-full h-full"}
+              w="256"
+            />
+          </div>
 
           {/* Details and Title */}
           <div className="w-2/3 my-auto">
             {/* Details */}
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold lg:text-lg">03.</h1>
-              <Link className="text-gray-200">Databases</Link>
+              <Link className="text-gray-300">Databases</Link>
               <span className="text-gray-300 text-sm">5 days ago</span>
             </div>
 
@@ -84,17 +94,20 @@ const FeaturedPost = () => {
 
         {/* Fourth */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured4.jpeg"
-            className={"rounded-3xl object-cover w-1/3 aspect-video"}
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured4.jpeg"
+              className={"rounded-3xl object-cover w-full h-full"}
+              w="256"
+            />
+          </div>
 
           {/* Details and Title */}
           <div className="w-2/3 my-auto">
             {/* Details */}
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold lg:text-lg">04.</h1>
-              <Link className="text-gray-200">Marketing</Link>
+              <Link className="text-gray-300">Marketing</Link>
               <span className="text-gray-300 text-sm">1 days ago</span>
             </div>
 
