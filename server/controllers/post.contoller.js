e7341c1c4977e1f6401f73c8eacb5af101ed1761
AmithBV0606 <amithrao0606@gmail.com>
2024-12-23 12:50:38 +0530

    Wrote code for generating the slug.

--- server/controllers/post.contoller.js
+++ server/controllers/post.contoller.js
@@ -24,7 +24,19 @@ export const createPost = async (req, res) => {
     return res.status(404).json("User not found!!");
   }
 
-  const newPost = await Post.create({ user: user._id, ...req.body });
+  const slug = req.body.title.replace(/ /g, "-").toLowerCase();
+
+  let existingPost = await Post.findOne({ slug });
+
+  let counter = 2;
+
+  while (existingPost) {
+    slug = `${slug}-${counter}`;
+    existingPost = await Post.findOne({ slug });
+    counter++;
+  }
+
+  const newPost = await Post.create({ user: user._id, slug, ...req.body });
   res.status(200).json(newPost);
 };
 
@@ -43,7 +55,7 @@ export const deletePost = async (req, res) => {
   });
 
   if (!deletePost) {
-    return res.status(403).json("You can delete only your posts!")
+    return res.status(403).json("You can delete only your posts!");
   }
 
   res.status(200).json("Post has been deleted!!");
