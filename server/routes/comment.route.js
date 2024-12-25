49a600b5b4d4020709e45f71624a77db6e4a09fa
AmithBV0606 <amithrao0606@gmail.com>
2024-12-25 18:01:38 +0530

    Created controllers and route handlers for performing the CRUD operations on comment.

--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,9 +1,14 @@
 import express from "express";
+import {
+  addComment,
+  deleteComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
-router.get("/comment", (req, res) => {
-    res.status(200).send("Comments router");
-});
+router.get("/:postId", getPostComments);
+router.post("/:postId", addComment);
+router.delete("/:id", deleteComment);
 
 export default router;
